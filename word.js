45be1ae1a63b292de4341b0774d1372efbee082a
game <anishsingh77177@gmail.com>
2023-10-17 15:45:56 +0530

    count

---
+++ word.js
@@ -0,0 +1,23 @@
+let inputText = document.querySelector(".input-text");
+let wordCount = document.querySelector(".word-count");
+let characterCount = document.querySelector(".character-count");
+let sentenceCount = document.querySelector(".sentence-count");
+let paragrapheCount = document.querySelector(".paragraph-count");
+
+inputText.addEventListener("input",() => {
+    // characterCount.textContent = inputText.value.length;
+    characterCount.textContent = inputText.value.replace(/\s+/g, '').length;
+
+    // remove white space from text
+     let textTrim = inputText.value.trim();
+    //   word counbt
+
+    wordCount.textContent = textTrim.split(/\s+/).filter((item) => item).length;
+
+    //sentece count
+    sentenceCount.textContent = textTrim.split(".").filter((item) => item).length;
+    //  count para
+
+    paragrapheCount.textContent = textTrim.split("\n\n").filter((item) => item).length;
+});
+
